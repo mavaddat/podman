c8e8cb23cca28737494c4303d13caa8a1f5133c0
Aryanbhargava18 <aryanbhargava644@gmail.com>
2026-07-29 16:51:50 +0530

    pkg/machine: remove unused provider functions
    The InstalledProviders and SupportedProviders functions were only
    used in their own tests and were otherwise dead code. Removing them
    entirely cleans up the API surface.
    
    Signed-off-by: Aryanbhargava18 <aryanbhargava644@gmail.com>

--- pkg/machine/provider/platform_darwin.go
+++ pkg/machine/provider/platform_darwin.go
@@ -51,11 +51,6 @@ func GetAll() []vmconfigs.VMProvider {
 	return []vmconfigs.VMProvider{new(libkrun.LibKrunStubber), new(applehv.AppleHVStubber)}
 }
 
-// SupportedProviders returns the providers that are supported on the host operating system
-func SupportedProviders() []define.VMType {
-	return []define.VMType{define.AppleHvVirt, define.LibKrun}
-}
-
 func IsInstalled(provider define.VMType) (bool, error) {
 	switch provider {
 	case define.AppleHvVirt:
